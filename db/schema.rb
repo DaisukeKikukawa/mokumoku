192185bbcf4d9b0e6f3e0192ac5c459068cb3386
DaisukeKikukawa <daisuke.kikukawa.16@gmail.com>
2022-08-15 17:23:45 +0900

    Fix: group,group_userそれぞれのマイグレーションファイルを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_072358) do
+ActiveRecord::Schema.define(version: 2022_08_15_081002) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,24 @@ ActiveRecord::Schema.define(version: 2022_01_19_072358) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "group_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["user_id"], name: "index_group_users_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.string "image_id"
+    t.integer "owner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "notification_timings", force: :cascade do |t|
     t.integer "timing", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -148,6 +166,8 @@ ActiveRecord::Schema.define(version: 2022_01_19_072358) do
   add_foreign_key "event_attendances", "users"
   add_foreign_key "events", "prefectures"
   add_foreign_key "events", "users"
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
   add_foreign_key "notifications", "users", column: "receiver_id"
   add_foreign_key "notifications", "users", column: "sender_id"
   add_foreign_key "user_notification_timings", "notification_timings"
