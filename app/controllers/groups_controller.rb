3e1c0e79b93b41a95ad9f22e232ea04cf3d19edf
DaisukeKikukawa <daisuke.kikukawa.16@gmail.com>
2022-08-18 18:32:37 +0900

    Fix: 文言の修正とグループ脱退の追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -44,6 +44,12 @@ class GroupsController < ApplicationController
     end
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user)
+    redirect_to groups_path
+  end
+
   private
 
   def group_params
