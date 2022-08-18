c01ce8088c07529aa4771ac6cf7811624c2c2af5
DaisukeKikukawa <daisuke.kikukawa.16@gmail.com>
2022-08-19 00:48:06 +0900

    Fix: roleカラムの追加

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,4 +1,5 @@
 class GroupUser < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  enum role: { general: 0, owner: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_17_031349) do
+ActiveRecord::Schema.define(version: 2022_08_18_144320) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 2022_08_17_031349) do
     t.integer "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0, null: false
     t.index ["group_id"], name: "index_group_users_on_group_id"
     t.index ["user_id"], name: "index_group_users_on_user_id"
   end
